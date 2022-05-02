cbda4deca71a830ffa9b6c0d9b938685e463caa5
NtshVrm <nitishvarma2k@gmail.com>
2022-05-03 00:27:38 +0530

    Update ListingPage.jsx

--- src/pages/ListingPage/ListingPage.jsx
+++ src/pages/ListingPage/ListingPage.jsx
@@ -7,10 +7,8 @@ import {
 } from "@fortawesome/free-regular-svg-icons";
 import {
   faBars,
-  faEllipsisVertical,
   faHistory,
   faHome,
-  faPlay,
   faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
